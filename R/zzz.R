be798f8f38585662e9391294332f9f4ad5ca3256
Stephen Turner <vustephen@gmail.com>
2023-02-21 15:18:21 -0500

    check that you're using git and in a package dir fixes #30

---
+++ R/zzz.R
@@ -0,0 +1,11 @@
+.onAttach <- function(libname, pkgname) {
+
+  # Are you using git?
+  isgit <- fs::dir_exists(".git")
+  if (isgit) packageStartupMessage(glue::glue("{pkgname} writes files. Best to use git when using pracpac functions."))
+
+  # Is this a package?
+  ispkg <- inherits(try(rprojroot::find_package_root_file(), silent=TRUE), "try-error")
+  if (ispkg) packageStartupMessage(glue::glue("{pkgname} is best used within a package directory. See Vignettes."))
+
+}
