366acb2bd6ab003145267bc06e9de5ebb8303390
Stephen Turner <vustephen@gmail.com>
2023-03-08 09:59:59 -0500

    consent renv onAttach

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,14 @@
 .onAttach <- function(libname, pkgname) {
 
+  # Consent renv
+  options(renv.consent = TRUE)
+
   # Are you using git?
   isgit <- fs::dir_exists(".git")
   if (!isgit) packageStartupMessage(glue::glue("{pkgname} writes files. Best to use git when using pracpac functions."))
 
   # Is this a package?
   ispkg <- !inherits(try(rprojroot::find_package_root_file(), silent=TRUE), "try-error")
-  if (!ispkg) packageStartupMessage(glue::glue("{pkgname} is best used within a package directory. See Vignettes."))
+  if (!ispkg) packageStartupMessage(glue::glue("{pkgname} is typically used within a package directory. See Vignettes."))
 
 }
