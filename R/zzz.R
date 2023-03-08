0b1a14c11fbc16eeab09f96ccfee7215c60202f6
Stephen Turner <vustephen@gmail.com>
2023-03-08 13:07:10 -0500

    remove automatic consent

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,5 @@
 .onAttach <- function(libname, pkgname) {
 
-  # Consent renv
-  options(renv.consent = TRUE)
-
   # Are you using git?
   isgit <- fs::dir_exists(".git")
   if (!isgit) packageStartupMessage(glue::glue("{pkgname} writes files. Best to use git when using {pkgname} functions."))
