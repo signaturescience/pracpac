af4a9bac2467bb85084c62f42fa594722b5baa35
Stephen Turner <vustephen@gmail.com>
2023-03-10 05:12:45 -0500

    inline docs

--- R/docker.R
+++ R/docker.R
@@ -107,7 +107,9 @@ add_dockerfile <- function(pkg_path = ".", img_path = NULL, use_renv = TRUE, use
   # Check that path is a package
   info <- pkg_info(pkg_path)
 
-  # Turn the string vector: c("a", "b", "c") to the single element string "'a','b','c'"
+  # Turn the string vector: c("a", "b", "c") to the single element string "'a','b','c'".
+  # If info$pkgdeps is empty, create a string 'character(0)' that gets glued into the dockerfile template.
+  # Note this must be a quoted string 'character(0)', not an empty string character(0), for the glue to work properly.
   if (length(info$pkgdeps==0L)) {
     pkgs <- paste(paste0("'",info$pkgdeps,"'"), collapse=",")
   } else {
